96428816b7c6d1809be63f07ae7bbaa83d2f74ef
Todd Waits <todd.waits@gmail.com>
2019-02-22 17:48:11 -0600

    Added wrapper for decorator to take argument for dynamic header.

--- decorators.py
+++ decorators.py
@@ -10,40 +10,45 @@ strength = {"Slayer": 10, "Witch": 4, "Whiner": 2, "Ex-Demon": 8}
 wisdom = {"Slayer": 6, "Witch": 9, "Whiner": 2, "Ex-Demon": 4}
 
 
-def heading(func):
-    def new_func(*args, **kwargs):
-        func(*args, **kwargs)
-        print()
+def heading(msg):
+    def dec(func):
+        def new_func(*args, **kwargs):
+            print(msg)
+            print("=====")
+            func(*args, **kwargs)
+            print()
 
-    return new_func
+        return new_func
+
+    return dec
 
 
-@heading
+@heading("Characters")
 def list_characters():
     for c in data:
         print(c["name"])
 
 
-@heading
+@heading("Roles")
 def list_roles():
     for c in data:
         print("%s: %s" % (c["name"], c["role"]))
 
 
-@heading
+@heading("Weapons")
 def list_weapons():
     for c in data:
         print("%s: %s" % (c["name"], c["weapon"]))
 
 
-@heading
+@heading("Stronger than x")
 def list_stronger():
     for c in data:
         if strength[c["role"]] > 6:
             print("%s: %s" % (c["name"], strength[c["role"]]))
 
 
-@heading
+@heading("Wiser than x")
 def list_wiser():
     for c in data:
         if wisdom[c["role"]] > 6:
@@ -51,24 +56,10 @@ def list_wiser():
 
 
 def main():
-    print("Characters")
-    print("==========")
     list_characters()
-
-    print("Roles")
-    print("=====")
     list_roles()
-
-    print("Weapons")
-    print("=======")
     list_weapons()
-
-    print("Stronger than 6")
-    print("===============")
     list_stronger()
-
-    print("Wiser than 6")
-    print("===============")
     list_stronger()
 
 
