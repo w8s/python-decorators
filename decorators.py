4c9a70ddbcbcb7ea17289c9d551e93617ac5a3c3
Todd Waits <todd.waits@gmail.com>
2019-02-26 16:47:14 -0600

    Converted for loops to list comprehensions.

--- decorators.py
+++ decorators.py
@@ -17,7 +17,7 @@ def heading(msg):
                 print(msg % args[0])
             except:
                 print(msg)
-            print('=' * len(msg))
+            print("=" * len(msg))
             func(*args, **kwargs)
             print()
 
@@ -28,34 +28,35 @@ def heading(msg):
 
 @heading("Characters")
 def list_characters():
-    for c in data:
-        print(c["name"])
+    [print(c["name"]) for c in data]
 
 
 @heading("Roles")
 def list_roles():
-    for c in data:
-        print("%s: %s" % (c["name"], c["role"]))
+    [print("%s: %s" % (c["name"], c["role"])) for c in data]
 
 
 @heading("Weapons")
 def list_weapons():
-    for c in data:
-        print("%s: %s" % (c["name"], c["weapon"]))
+    [print("%s: %s" % (c["name"], c["weapon"])) for c in data]
 
 
 @heading("Stronger than %d")
 def list_stronger(strength_target):
-    for c in data:
-        if strength[c["role"]] > strength_target:
-            print("%s: %s" % (c["name"], strength[c["role"]]))
+    [
+        print("%s: %s" % (c["name"], strength[c["role"]]))
+        for c in data
+        if strength[c["role"]] > strength_target
+    ]
 
 
 @heading("Wiser than %d")
 def list_wiser(wisdom_target):
-    for c in data:
-        if wisdom[c["role"]] > wisdom_target:
-            print("%s: %s" % (c["name"], wisdom[c["role"]]))
+    [
+        print("%s: %s" % (c["name"], wisdom[c["role"]]))
+        for c in data
+        if wisdom[c["role"]] > wisdom_target
+    ]
 
 
 def main():
